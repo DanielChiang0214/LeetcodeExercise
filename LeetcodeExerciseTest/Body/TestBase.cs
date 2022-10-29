5218b183a3cf0f19c5054190bf7135568d87b60a
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-10-29 16:05:23 +0800

    ✨ 添加TestBase

---
+++ LeetcodeExerciseTest/Body/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest.Body
+{
+    public class TestBase
+    {
+        public ITestOutputHelper Output { get; }
+        public TestBase(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        public T Print<T>(T @object)
+        {
+            Output.WriteLine(@object.ToString());
+            return @object;
+        }
+    }
+}
