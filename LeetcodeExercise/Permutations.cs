c533f4c705eef22ab7dd0d3ae765f8c8e9a73fa0
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-10-30 12:16:35 +0800

    ✨ Permutations

---
+++ LeetcodeExercise/Permutations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeExerciseTest
+{
+    public class Permutations
+    {
+        public static IEnumerable<IEnumerable<int>> Permute(IEnumerable<int> nums)
+        {
+            if (!nums.Any())
+            {
+                yield return Enumerable.Empty<int>();
+                yield break;
+            }
+            foreach (var item in nums)
+            {
+                foreach (var ienumerable in Permute(nums.Where(o => o != item)))
+                {
+                    yield return ienumerable.Prepend(item);
+                }
+            }
+        }
+    }
+}
---
+++ LeetcodeExerciseTest/PermutationsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using LeetcodeExerciseTest.Body;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest
+{
+
+    public class PermutationsTest : TestBase
+    {
+        public PermutationsTest(ITestOutputHelper output) : base(output)
+        {
+
+        }
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 })]
+        [InlineData(new[] { 1, 10, 5, 7 })]
+        [InlineData(new[] { 1, 20, 10, 7 })]
+        public void Permute_Validation_Logic(int[] nums)
+        {
+            foreach (var ienumerable in Permutations.Permute(nums))
+            {
+                Output.WriteLine(string.Join(",", ienumerable));
+            }
+        }
+
+    }
+}
