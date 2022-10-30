1c144c51604e21a472821c9f326e7166a401e5f0
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-10-30 12:29:50 +0800

    ✨ GenerateParentheses

---
+++ LeetcodeExercise/GenerateParentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    public class GenerateParentheses
+    {
+        private IList<string> res = new List<String>();
+
+        public void Backtrack(ref int n, int open, int close, string temp)
+        {
+            if (open > n || close > open)
+            {
+                return;
+            }
+            if (temp.Length == n * 2)
+            {
+                res.Add(temp);
+                return;
+            }
+            Backtrack(ref n, open + 1, close, temp + '(');
+            Backtrack(ref n, open, close + 1, temp + ')');
+        }
+        public IList<string> GenerateParenthesis(int n)
+        {
+            Backtrack(ref n, 0, 0, "");
+            return res;
+        }
+    }
+}
