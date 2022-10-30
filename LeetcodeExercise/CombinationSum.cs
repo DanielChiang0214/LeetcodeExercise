d35ff4e807176a106d60d120eb0063adc15c997e
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-10-30 12:05:13 +0800

    ✨ CombinationSum

---
+++ LeetcodeExercise/CombinationSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeExerciseTest
+{
+    public class CombinationSum     
+    {
+        public static IList<IList<int>> Execute(int[] candidates, int target)
+        {
+            var result = new List<IList<int>>();
+            Execute(candidates, Array.Empty<int>(), target, result);
+            return result;
+        }
+        public static void Execute(int[] candidates, int[] body, int target, IList<IList<int>> @return)
+        {
+            var temp = body.Sum();
+            if (temp == target)
+            {
+                @return.Add(body.ToList());
+            }
+
+            foreach (var item in candidates.Where(i => i >= body.LastOrDefault()).Where(i => i <= target - temp))
+            {
+                Execute(candidates, body.Append(item).ToArray(), target, @return);
+            }
+        }
+    }
+}
---
+++ LeetcodeExerciseTest/CombinationSumTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetcodeExerciseTest.Body;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest
+{
+
+    public class CombinationSumTest : TestBase
+    {
+        public CombinationSumTest(ITestOutputHelper output) : base(output)
+        {
+        }
+        [Theory]
+        [InlineData(new[] { 1, 3, 5, 7 }, 4)]
+        [InlineData(new[] { 2, 3, 6, 7 }, 7)]
+        [InlineData(new[] { 2, 3, 5 }, 8)]
+        public void CombinationSum_Validation_Logic(int[] candidates, int target)
+        {
+            var result = new List<IList<int>>();
+            CombinationSum.Execute(candidates, Array.Empty<int>(), target, result);
+
+            foreach (var item in result)
+            {
+                Output.WriteLine(string.Join(",", item));
+            }
+        }
+    }
+}
