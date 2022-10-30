72c1d511510d887f4f3fc67793dd63d02e5c1dad
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-10-30 12:27:09 +0800

    ✨ JumpGame

---
+++ LeetcodeExercise/JumpGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    public class JumpGame
+    {
+        public static bool CanJump(int[] nums)
+        {
+
+            int f = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > f)
+                    return false;
+
+                f = Math.Max(f, i + nums[i]);
+                if (f > nums.Length)
+                    return true;
+            }
+
+            return true;
+        }
+    }
+}
