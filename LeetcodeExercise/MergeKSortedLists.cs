f21363e2820e518e0327443e9476fcc0d317bb62
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-11-04 00:05:21 +0800

    ✨ MergeKSortedLists

---
+++ LeetcodeExercise/MergeKSortedLists.cs
@@ -0,0 +1,54 @@
+﻿using LeetcodeExerciseTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    public class MergeKSortedLists
+    {
+        public static ListNode MergeKLists(ListNode[] lists)
+        {
+            var minNode = default(ListNode);
+            var dict = new Dictionary<int, List<ListNode>>();
+            foreach (var item in lists)
+            {
+                if (item is null)
+                {
+                    continue;
+                }
+                if (dict.TryGetValue(item.val, out var listNodes) == false)
+                {
+                    if (minNode is null || minNode.val > item.val)
+                    {
+                        minNode = item;
+                    }
+                    dict[item.val] = listNodes = new();
+                }
+                listNodes.Add(item);
+            }
+            if (minNode is null)
+            {
+                return minNode!;
+            }
+            var nextItem = Enumerable.Empty<ListNode>();
+            var minlist = dict[minNode.val];
+            dict.Remove(minNode.val);
+            minNode = new ListNode();
+            var current = minNode;
+            foreach (var item in minlist)
+            {
+                if (item.next is not null)
+                {
+                    nextItem = nextItem.Append(item.next);
+                }
+                current.next = item;
+                current = current.next;
+            }
+            current.next = MergeKLists(nextItem.Concat(dict.SelectMany(kvp => kvp.Value)).ToArray());
+            return minNode.next;
+        }
+    }
+}
---
+++ LeetcodeExerciseTest/MergeKSortedListsTest.cs
@@ -0,0 +1,28 @@
+﻿using LeetcodeExercise;
+using LeetcodeExerciseTest.Body;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest
+{
+    public class MergeKSortedListsTest : TestBase
+    {
+        public MergeKSortedListsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 1, 3, 5, 7, 9 })]
+        public void CombinationSum_Validation_Logic(params int[][] candidates)
+        {
+            var source = candidates.Select(item => ListNode.Create(item)).ToArray();
+            var dd = MergeKSortedLists.MergeKLists(source);
+
+            Output.WriteLine(dd.ToString());
+        }
+    }
+}
