7e52fa182636629f7a3fd8fdf597d26aed336969
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-11-04 09:02:14 +0800

    ✨ MedianOfTwoSortedArrays

---
+++ LeetcodeExercise/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    public class MedianOfTwoSortedArrays
+    {
+        public static decimal FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var margeIndex = 0;
+            var i = 0;
+            var j = 0;
+            var marge = new int[nums1.Length + nums2.Length];
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                marge[margeIndex++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+            }
+            while (i < nums1.Length)
+            {
+                marge[margeIndex++] = nums1[i++];
+            }
+            while (j < nums2.Length)
+            {
+                marge[margeIndex++] = nums2[j++];
+            }
+
+            return marge.Length % 2 == 0 ? (marge[marge.Length / 2] + marge[(marge.Length - 1) / 2]) / 2.0m : marge[marge.Length / 2];
+        }
+    }
+}
---
+++ LeetcodeExerciseTest/MedianOfTwoSortedArraysTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetcodeExercise;
+using LeetcodeExerciseTest.Body;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest
+{
+    public class MedianOfTwoSortedArraysTest : TestBase
+    {
+        public MedianOfTwoSortedArraysTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 3 }, new[] { 2 }, 2)]
+        [InlineData(new[] { 1, 2 }, new[] { 3, 4 }, 2.5)]
+        public void CombinationSum_Validation_Logic(int[] array1, int[] array2, decimal anser)
+        {
+            var value = MedianOfTwoSortedArrays.FindMedianSortedArrays(array1, array2);
+
+            Assert.Equal(value, anser);
+        }
+    }
+}
