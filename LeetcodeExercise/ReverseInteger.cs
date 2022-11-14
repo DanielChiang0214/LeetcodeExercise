e0dc6b99510b640c61e66febf32d2193be5650d2
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-11-14 09:00:58 +0800

    ✨ ReverseInteger

---
+++ LeetcodeExercise/ReverseInteger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    internal class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var result = 0;
+            while (x != 0)
+            {
+                var temp = x % 10;
+                x = x / 10;
+                var tt = result * 10L + temp;
+                if (tt > int.MaxValue || tt < int.MinValue)
+                {
+                    return 0;
+                }
+                else
+                {
+                    result = (int)tt;
+                }
+            }
+            return result;
+        }
+
+    }
+}
