0ede9a300c8c03f26721417c7ca786af86c1f6fe
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-11-15 12:49:38 +0800

    ✨ CountCompleteTreeNodes

---
+++ LeetcodeExercise/CountCompleteTreeNodes.cs
@@ -0,0 +1,19 @@
+﻿using LeetcodeExercise.Model;
+
+namespace LeetcodeExercise
+{
+    public class CountCompleteTreeNodes
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            var count = 1;
+            count += CountNodes(root.right);
+            count += CountNodes(root.left);
+            return count;
+        }
+    }
+}
