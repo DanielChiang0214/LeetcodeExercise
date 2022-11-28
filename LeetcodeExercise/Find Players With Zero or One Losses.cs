62daad7424a904f929da1c768cb5a71b2ef7f7a0
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-11-28 13:45:15 +0800

    ✨ Find Players With Zero or One Losses

---
+++ LeetcodeExercise/Find Players With Zero or One Losses.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetcodeExercise
+{
+	public class Find_Players_With_Zero_or_One_Losses
+	{
+		public IList<IList<int>> FindWinners(int[][] matches)
+		{
+			var key = 0;
+			var statistics = new Dictionary<int, int>();
+			for (int i = 0; i < matches.Length; i++)
+			{
+				key = matches[i][0];
+				statistics[key] = statistics.TryGetValue(key, out var last) ? last : 0;
+				key = matches[i][1];
+				statistics[key] = statistics.TryGetValue(key, out var result) ? ++result : 1;
+			}
+
+			var noDefeat = new List<int>();
+			var failedOnce = new List<int>();
+			foreach (var kvp in statistics)
+			{
+				if (kvp.Value == 0)
+				{
+					noDefeat.Add(kvp.Key);
+				}
+				if (kvp.Value == 1)
+				{
+					failedOnce.Add(kvp.Key);
+				}
+			}
+			noDefeat.Sort();
+			failedOnce.Sort();
+			return new List<IList<int>>()
+			{
+				noDefeat,
+				failedOnce
+			};
+		}
+	}
+}
---
+++ LeetcodeExerciseTest/Find_Players_With_Zero_or_One_LossesTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetcodeExercise;
+using LeetcodeExerciseTest.Body;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetcodeExerciseTest
+{
+	public class Find_Players_With_Zero_or_One_LossesTest : TestBase
+	{
+		public Find_Players_With_Zero_or_One_LossesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Theory]
+		[InlineData(new int[] { 1, 2, 10 }, new int[] { 4, 5, 7, 8 }, new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 3, 6 }, new int[] { 5, 6 }, new int[] { 5, 7 }, new int[] { 4, 5 }, new int[] { 4, 8 }, new int[] { 4, 9 }, new int[] { 10, 4 }, new int[] { 10, 9 })]
+		[InlineData(new int[] { 1 }, new int[] { 100000 }, new int[] { 1, 100000 })]
+		public void TTTTTT(int[] noDefeat, int[] failedOnce, params int[][] matches)
+		{
+			for (int i = 0; i < matches.Length; i++)
+			{
+				Output.WriteLine($"[{matches[i][0]},{matches[i][1]}]");
+			}
+			var result = new Find_Players_With_Zero_or_One_Losses().FindWinners(matches);
+			Assert.Equal(noDefeat, result.First());
+			Assert.Equal(failedOnce, result.Skip(1).First());
+		}
+	}
+}
