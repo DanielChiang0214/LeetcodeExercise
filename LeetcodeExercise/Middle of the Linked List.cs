32fb3bfa329b62141d0df38828d639aa1a080d8d
daniel_chiang <daniel_chiang@cmoney.com.tw>
2022-12-05 08:17:39 +0800

    ✨ Middle_of_the_Linked_List

---
+++ LeetcodeExercise/Middle of the Linked List.cs
@@ -0,0 +1,27 @@
+﻿using LeetcodeExerciseTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+	public class Middle_of_the_Linked_List
+	{
+		public ListNode MiddleNode(ListNode head)
+		{
+			var index = 0;
+			var middle = head;
+			while (head.next is not null)
+			{	
+				head = head.next;
+				if (++index % 2 == 1)
+				{
+					middle = middle.next;
+				}
+			}
+			return middle;
+		}
+	}
+}
