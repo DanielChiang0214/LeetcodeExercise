5566674c4cc3b2d5f5fbf90978a22359eefff325
daniel_chiang <daniel_chiang@cmoney.com.tw>
2023-01-04 09:09:25 +0800

    ✨ 2244. Minimum Rounds to Complete All Tasks

---
+++ LeetcodeExercise/Minimum Rounds to Complete All Tasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    public class Minimum_Rounds_to_Complete_All_Tasks
+    {
+        public int MinimumRounds(int[] tasks)
+        {
+            var operate = 0;
+            var count = 0;
+            foreach (var item in tasks.GroupBy(item => item))
+            {
+                count = item.Count();
+                if (count < 2)
+                {
+                    return -1;
+                }
+                operate += count % 3 == 0 ? count / 3 : count / 3 + 1;
+            }
+            return operate;
+        }
+    }
+}
