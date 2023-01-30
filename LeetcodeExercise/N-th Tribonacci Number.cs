9344b700ed348e3938b23f8f9bb444cc2d04db82
daniel_chiang <daniel_chiang@cmoney.com.tw>
2023-01-30 11:59:52 +0800

    ✨ 1137. N-th Tribonacci Number

---
+++ LeetcodeExercise/N-th Tribonacci Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    internal class N_th_Tribonacci_Number
+    {
+        public int Tribonacci(int n)
+        {
+            if (n < 3)
+            {
+                return n;
+            }
+            var x1 = 0;
+            var x2 = 1;
+            var x3 = 1;
+
+            while (n-- > 0)
+            {
+                (x1, x2, x3) = (x2, x3, x1 + x2 + x3);
+            }
+            return x1;
+        }
+    }
+}
