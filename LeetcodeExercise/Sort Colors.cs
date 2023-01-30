81df0c169fdcb7c490e1b612c18c10bbb366487e
daniel_chiang <daniel_chiang@cmoney.com.tw>
2023-01-30 13:14:18 +0800

    ✨ 75. Sort Colors

---
+++ LeetcodeExercise/Sort Colors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    internal class Sort_Colors
+    {
+        public void SortColors(int[] nums)
+        {
+            if (nums.Any() == false)
+            {
+                return;
+            }
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+                }
+            }
+        }
+    }
+}
