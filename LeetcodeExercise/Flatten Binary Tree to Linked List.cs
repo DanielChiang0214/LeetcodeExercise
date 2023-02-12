fdd62c2640ff8a8d321677c6bf8c26874ee72cf1
daniel_chiang <daniel_chiang@cmoney.com.tw>
2023-02-12 16:20:59 +0800

    ✨ 114.Flatten Binary Tree to Linked List

---
+++ LeetcodeExercise/Flatten Binary Tree to Linked List.cs
@@ -0,0 +1,35 @@
+﻿using LeetcodeExercise.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeExercise
+{
+    internal class Flatten_Binary_Tree_to_Linked_List
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root is null)
+            {
+                return;
+            }
+            if (root.left is not null)
+            {
+                if (root.right is not null)
+                {
+                    var temp = root.left;
+                    while (temp.right is not null)
+                    {
+                        temp = temp.right;
+                    }
+                    temp.right = root.right;
+                }
+                root.right = root.left;
+                root.left = default!;
+            }
+            Flatten(root.right);
+        }
+    }
+}
